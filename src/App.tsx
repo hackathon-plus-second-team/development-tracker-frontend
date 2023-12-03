11d875b9ab8fe9ffd2eb69a8366a0409b551dbc5
Kikodze Viktor <vitland-x@yandex.ru>
2023-12-03 15:39:41 +0300

    feat:  Add icons and styles.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import { useState } from 'react';
+import Dropdown from './ui-kit/Dropdown/Dropdown';
+import { SelectChangeEvent } from '@mui/material';
+
+const val = [
+    { id: 1, label: 'ffdf' },
+    { id: 2, label: '333ffdf' },
+    { id: 3, label: '2222ffdf' },
+];
+
 function App() {
-    return <></>;
+    const [selectedCourse, setSelectedCourse] = useState<string[]>([]);
+    const handleCourseChange = (evt: SelectChangeEvent<string[]>) => {
+        const {
+            target: { value },
+        } = evt;
+
+        setSelectedCourse(typeof value === 'string' ? value.split(',') : value);
+    };
+    return (
+        <>
+            <Dropdown options={val} onChange={handleCourseChange} selectedValues={selectedCourse} placeholder="Все мои навыки" />
+        </>
+    );
 }
 
 export default App;
--- src/ui-kit/Dropdown/Dropdown.tsx
+++ src/ui-kit/Dropdown/Dropdown.tsx
@@ -1,32 +1,51 @@
 import Select, { SelectProps } from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 import { ListItemIcon, ListItemText } from '@mui/material';
-import CheckIcon from '@mui/icons-material/Check';
+import CheckIcon from '../../assets/img/check_mini.svg?react';
+import ArrowIcon from '../../assets/img/arrow_down.svg?react';
+import styles from './Dropdown.module.scss';
 type TValues = {
     id: number;
-    lable: string;
+    label: string;
 };
 type TDropdonwProps = {
     options: TValues[];
-    selectedValues: string[]
+    selectedValues: string[];
+    placeholder: string;
 };
 
-const Dropdown = ({ options, selectedValues, ...rest}: TDropdonwProps & SelectProps) => {
-
+const Dropdown = ({ options, selectedValues, placeholder, ...rest }: TDropdonwProps & SelectProps<string[]>) => {
     return (
         <Select
-        size="small"
-        sx={{ minWidth: '320px' }}
-        inputProps={{ 'aria-label': 'Without label' }}
-        value={selectedValues}
-        multiple
-        {...rest}
+            size="small"
+            sx={{ minWidth: '320px' }}
+            multiple
+            value={selectedValues}
+            displayEmpty
+            inputProps={{ 'aria-label': 'Without label' }}
+            IconComponent={(props) => <ArrowIcon {...props} />}
+            MenuProps={{
+                PaperProps: { sx: { left: `8px !important` } },
+            }}
+            renderValue={(values: Array<string>) =>
+                values.length ? (
+                    values
+                        .map((id) => {
+                            const selected = options.find((opt) => opt.id.toString() == id);
+                            return selected ? selected.label : id;
+                        })
+                        .join(', ')
+                ) : (
+                    <em className={styles.placeholder}>{placeholder}</em>
+                )
+            }
+            {...rest}
         >
-            {options.map(({ id, lable }) => {
-                const selected = selectedValues.includes(id.toString())
+            {options.map(({ id, label }) => {
+                const selected = selectedValues.find((opt) => opt == id.toString());
                 return (
                     <MenuItem key={id} value={id}>
-                        <ListItemText primary={lable} />
+                        <ListItemText primary={label} />
                         {selected && (
                             <ListItemIcon>
                                 <CheckIcon />
@@ -38,4 +57,5 @@ const Dropdown = ({ options, selectedValues, ...rest}: TDropdonwProps & SelectPr
         </Select>
     );
 };
+
 export default Dropdown;
