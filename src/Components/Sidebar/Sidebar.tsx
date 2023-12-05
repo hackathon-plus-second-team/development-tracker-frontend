7876d71ab9dc7d7b523f86e6fce3e8a968a6308c
Kikodze Viktor <vitland-x@yandex.ru>
2023-12-05 20:09:36 +0300

    feat: Add Sidebar

--- src/Components/Sidebar/Sidebar.tsx
+++ src/Components/Sidebar/Sidebar.tsx
@@ -1,6 +1,19 @@
+import styles from "./Sidebar.module.scss"
+import {Link} from "react-router-dom"
+import {UserIcon} from '../../assets/img/user.svg?react'
+
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <aside className={styles.sidebar}>
+        <div className={styles.profile}></div>
+        <nav>
+            <div className={styles.linksContainer}>
+                <div className={styles.linkContainer}>
+                    <Link className={styles.link}></Link>
+                </div>
+            </div>
+        </nav>
+    </aside>
   )
 }
 export default Sidebar
