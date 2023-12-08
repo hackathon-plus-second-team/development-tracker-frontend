53295f3fdbb2e24f45475538afc2b75bc10c6e4b
Vitkor <vitland@gmail.com>
2023-12-08 19:15:04 +0300

    feature: add skillsSlice

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,13 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import authReducer from './authSlice';
 import coursesReducer from './coursesSlice';
+import skillsReducer from './skillsSlice';
 
 const store = configureStore({
     reducer: {
         auth: authReducer,
-        courses: coursesReducer
+        courses: coursesReducer,
+        skills: skillsReducer
     },
 });
 
