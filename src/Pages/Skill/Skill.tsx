0631265c379bb9b5b5a0d2d9282a94c4a62414f2
Vitkor <vitland@gmail.com>
2023-12-09 10:24:00 +0300

    style: change styling

--- src/Pages/Skill/Skill.tsx
+++ src/Pages/Skill/Skill.tsx
@@ -11,21 +11,26 @@ const Skill = () => {
     const dispatch = useAppDispatch();
     const skill = useAppSelector(getCurrentSkill);
     const location = useLocation();
-    const navigate = useNavigate()
+    const navigate = useNavigate();
     useEffect(() => {
         dispatch(getSkill(location.pathname.slice(8)));
     }, [dispatch]);
+
+    const btnText = skill && skill.level > 0 ? 'Пройти тест ещё раз' : 'Пройти тест';
 
     return (
         <section className={styles.skill}>
             {skill ? (
                 <>
-                    <BackArrow onClick={()=> navigate(-1)} className={styles.back}/>
+                    <BackArrow onClick={() => navigate(-1)} className={styles.back} />
                     <div className={styles.textContainer}>
                         <h2 className={styles.heading}>Разработчик</h2>
                         <h3 className={styles.subHeading}>{skill?.name}</h3>
                         <p className={styles.text}>{skill?.description}</p>
-                        <CustomButton text="Пройти тест" buttonVariant="contained" disabled={false} />
+                        <div className={styles.btnContainer}>
+                            <CustomButton text={btnText} buttonVariant="contained" disabled={false} />
+                            {skill.level < 100 && skill.level > 0 ? <CustomButton text="Прочитать материалы" buttonVariant="outlined" disabled={false} /> : null}
+                        </div>
                     </div>
                     <RoundProgressbar label="Навыки подтверждены" value={skill.level} />
                 </>
